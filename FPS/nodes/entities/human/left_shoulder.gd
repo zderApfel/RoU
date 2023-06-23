d784dd7fe2a6a3676ec5afc955b30a79e6fb58f6
zderApfel <scoutt172@yahoo.com>
2023-06-22 21:28:02 -0400

    I don't want to talk about it

---
+++ FPS/nodes/entities/human/left_shoulder.gd
@@ -0,0 +1,10 @@
+extends BoneAttachment3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.bone_name = "torso"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
