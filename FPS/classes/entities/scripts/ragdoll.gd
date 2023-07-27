0d2a35e1deca954121256aaf203eb8d110645571
zderApfel <scoutt172@yahoo.com>
2023-07-27 17:08:38 -0400

    Completed death animation/ragdoll

--- FPS/classes/entities/scripts/ragdoll.gd
+++ FPS/classes/entities/scripts/ragdoll.gd
@@ -1,9 +1,11 @@
 extends Skeleton3D
 
+@onready var COLLISION = get_tree().get_root().get_node("collision")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	physical_bones_start_simulation()
+	COLLISION = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
